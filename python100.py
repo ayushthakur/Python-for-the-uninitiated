404c8648cbad096ca4add3ea448ea42d43121f8b
ayushthakur <ayushthakur2293@gmail.com>
2016-03-05 15:30:09 +0530

    Create python100.py

---
+++ python100.py
@@ -0,0 +1,32 @@
+#Variable Declaration 
+#Integer and Float 
+a=9
+b=1.9
+
+#string
+c="Sunday"
+
+#Printing values on screeen 
+print(b)
+
+#Getting the type of the variable 
+t=type(c)         #store the type of the variable in t 
+print(t)          #Print the type of c
+print (type(c))   #Print the type of c
+
+
+#Arithmetic Operators 
+two=1+1
+zero=1-1
+sixteen=4*4
+eight=sixteen/two
+
+#Changing the type of a variable 
+int(1.6) #result: 1  int(): to convert to integer
+str(1) #result: '1'  #str()- to convert to string
+
+
+
+
+
+
