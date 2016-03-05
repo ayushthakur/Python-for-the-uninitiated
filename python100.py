f4ac1c872150fe82b6b523f52bef6c59214b3ac6
ayushthakur <ayushthakur2293@gmail.com>
2016-03-05 17:19:24 +0530

    Update python100.py

--- python100.py
+++ python100.py
@@ -25,6 +25,28 @@ eight=sixteen/two
 int(1.6) #result: 1  int(): to convert to integer
 str(1) #result: '1'  #str()- to convert to string
 
+#Lists : Object that represents sequence of values 
+
+l=[] #Create an empty list of name l, (l is an object of list class) 
+l.append("Monday") # Use append() method to add string "Monday" to list l 
+l.append(1) # Adding 1 to l, You can add different types of values in python here--> string,integer
+print(l) 
+#Result: ['Monday', 1]
+type(l) #Result: <type 'list'> 
+
+#Creating filled lists 
+l=["Monday","Tuesday","Wednesday"]
+p=[1,2,3]
+
+
+
+
+
+
+
+
+
+
 
 
 
